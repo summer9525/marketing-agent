1dd9b8311534d8f86f82a28abde57ee3eac7a4e6
Summer.유경원 <summer.yugyeong-won@Summeryugyeong-won-ui-MacBookPro.local>
2026-04-07 15:26:40 +0900

    fix: add basename to BrowserRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/marketing-agent">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
